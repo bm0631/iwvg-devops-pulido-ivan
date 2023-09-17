b8aa6eb66316170e8d4d1ef30e60bcea88585df3
Ivan Pulido <ivan.pulido.diaz@alumnos.upm.es>
2023-09-17 18:48:38 +0200

    new Methods FractionTest #10

--- src/test/java/es/upm/miw/iwvg_devops/rest/code/FractionTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/rest/code/FractionTest.java
@@ -4,7 +4,7 @@ import es.upm.miw.iwvg_devops.code.Fraction;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class FractionTest {
     Fraction fraction;
@@ -53,4 +53,63 @@ public class FractionTest {
                 '}';
         assertEquals(stringExpected, this.fraction.toString());
     }
+
+    @Test
+    void testIsPopper() {
+        assertTrue(this.fraction.isPopper());
+        this.fraction = new Fraction(1, 1);
+        assertTrue(this.fraction.isPopper());
+        this.fraction = new Fraction(5, 3);
+        assertFalse(this.fraction.isPopper());
+    }
+
+    @Test
+    void testIsImproper() {
+        assertFalse(this.fraction.isImproper());
+        this.fraction = new Fraction(4, 4);
+        assertFalse(this.fraction.isImproper());
+        this.fraction = new Fraction(6, 4);
+        assertTrue(this.fraction.isImproper());
+    }
+
+    @Test
+    void testIsEquivalent() {
+        Fraction fraction = new Fraction(4, 6);
+        assertTrue(this.fraction.isEquivalent(fraction));
+        Fraction fraction2 = new Fraction(1, 6);
+        assertFalse(this.fraction.isEquivalent(fraction2));
+    }
+
+    @Test
+    void testAdd() {
+        Fraction fraction = new Fraction(1, 3);
+        this.fraction.add(fraction);
+        assertEquals(3, this.fraction.getNumerator());
+        assertEquals(3, this.fraction.getDenominator());
+
+        Fraction fraction2 = new Fraction(1, 2);
+        this.fraction.add(fraction2);
+        assertEquals(9, this.fraction.getNumerator());
+        assertEquals(6, this.fraction.getDenominator());
+
+    }
+
+    @Test
+    void testMultiply() {
+        Fraction fraction = new Fraction(2, 3);
+        this.fraction.multiply(fraction);
+        assertEquals(4, this.fraction.getNumerator());
+        assertEquals(9, this.fraction.getDenominator());
+
+    }
+
+    @Test
+    void testDivide() {
+        Fraction fraction = new Fraction(2, 4);
+        this.fraction.divide(fraction);
+        assertEquals(8, this.fraction.getNumerator());
+        assertEquals(6, this.fraction.getDenominator());
+
+    }
+
 }
